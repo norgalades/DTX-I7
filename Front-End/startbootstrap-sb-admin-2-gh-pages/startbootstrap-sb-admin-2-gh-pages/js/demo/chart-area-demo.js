8e9a64af23f6ffcfa612b2807b1e59dc4717df3a
norgalades <30833739+norgalades@users.noreply.github.com>
2019-07-24 13:39:48 +0200

    added code for historical data

--- Front-End/startbootstrap-sb-admin-2-gh-pages/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-area-demo.js
+++ Front-End/startbootstrap-sb-admin-2-gh-pages/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-area-demo.js
@@ -1,7 +1,27 @@
+window.HistData = 0;
+function callAPIForHistoricalData() {
+    console.log("callAPIForHistoricalData");
+    $.ajax({
+        //url: 'https://a4girz51oh.execute-api.us-east-1.amazonaws.com/return/1?recordTime=1', //FIXME: get the actual link
+        url: 'https://1xwt8lhj3l.execute-api.eu-central-1.amazonaws.com/turbinestats/all?id=turbine',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            window.HistData = response.record;
+            createChart(window.HistData);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+
+    console.log(window.HistData);
+   
+   
+}
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
-
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
@@ -27,92 +47,142 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    datasets: [{
-      label: "Temperature",
-      lineTension: 0.3,
-      backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(78, 115, 223, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: [0, 10, 50, 15, 10, 20, 15, 25, 20, 300, 25, 40],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 7
+function createChart(histDataLocal) {
+    var count = [0,0,0,0];
+    var voltArray = [0,0,0,0];
+    var tempArray = [0,0,0,0];
+   
+    for (var i = 0; i < histDataLocal.length; i++) {
+        if (parseFloat(histDataLocal[i].voltage) == NaN)
+        {
+            parseFloat(histDataLocal[i].voltage) == 0;
         }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
+        if (histDataLocal[i].turbineId == 1) {
+            count[0] += 1;
+            voltArray[0] += parseFloat(histDataLocal[i].voltage);
+            tempArray[0] += parseFloat(histDataLocal[i].temp);
         }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        else if (histDataLocal[i].turbineId == 2){
+            count[1] += 1;
+            voltArray[1] += parseFloat(histDataLocal[i].voltage);
+            tempArray[1] += parseFloat(histDataLocal[i].temp);
         }
-      }
+        else if (histDataLocal[i].turbineId == 3) {
+            count[2] += 1;
+            voltArray[2] += parseFloat(histDataLocal[i].voltage);
+            tempArray[2] += parseFloat(histDataLocal[i].temp);
+        }
+        else if (histDataLocal[i].turbineId == 4) {
+            count[3] += 1;
+            voltArray[3] += parseFloat(histDataLocal[i].voltage);
+            tempArray[3] += parseFloat(histDataLocal[i].temp);
+        }
     }
-  }
-});
+  
+ 
+
+    var volLen = [];
+    for (var j = 0; j < voltArray.length; j++) {
+       
+        volLen[j] = j;
+    }
+    console.log(tempArray);
+    console.log(voltArray);
+    console.log(count);
+
+    voltArray[0] = voltArray[0] / count[0];
+    voltArray[1] = voltArray[1] / count[1];
+    voltArray[2] = voltArray[2] / count[2];
+
+    console.log(voltArray);
+   
+    // Area Chart Example
+    var ctx = document.getElementById("myAreaChart");
+    var myLineChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['1','2','3'],
+            datasets: [{
+                label: "Temperature",
+                lineTension: 0.3,
+                backgroundColor: "rgba(78, 115, 223, 0.05)",
+                borderColor: "rgba(78, 115, 223, 1)",
+                pointRadius: 3,
+                pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointBorderColor: "rgba(78, 115, 223, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: voltArray,
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'date'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 7
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function (value, index, values) {
+                            return '' + number_format(value);
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                intersect: false,
+                mode: 'index',
+                caretPadding: 10,
+                callbacks: {
+                    label: function (tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                    }
+                }
+            }
+        }
+    });
+}
--- Front-End/startbootstrap-sb-admin-2-gh-pages/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ Front-End/startbootstrap-sb-admin-2-gh-pages/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -1,99 +1,151 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+window.HistData = 0;
+function callAPIForHistoricalDataLine() {
+    console.log("callAPIForHistoricalData");
+    $.ajax({
+        //url: 'https://a4girz51oh.execute-api.us-east-1.amazonaws.com/return/1?recordTime=1', //FIXME: get the actual link
+        url: 'https://1xwt8lhj3l.execute-api.eu-central-1.amazonaws.com/turbinestats/all?id=turbine',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            window.HistData = response.record;
+            createChartLine(window.HistData);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
 
-// Pie Chart Example
+    console.log(window.HistData);
 
-var ctx = document.getElementById("myPieChart");
-var myBarChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["1", "2", "3", "4", "5", "6"],
-        datasets: [{
-            label: "Temperature",
-            hoverBackgroundColor: "#2e59d9",
-            borderColor: "#4e73df",
-            data: [10, 20, 30, 40, 10, 25],
-        }],
-    },
-    options: {
-        maintainAspectRatio: false,
-        layout: {
-            padding: {
-                left: 10,
-                right: 25,
-                top: 25,
-                bottom: 0
-            }
-        },
-        scales: {
-            xAxes: [{
-                time: {
-                    unit: 'Temperature'
-                },
-                gridLines: {
-                    display: false,
-                    drawBorder: false
-                }
-                
-            }],
-            yAxes: [{
-                ticks: {
-                    min: 0,
-                    max: 150,
-                    maxTicksLimit: 5,
-                    padding: 10,
-                    // Include a dollar sign in the ticks
-                    callback: function (value, index, values) {
-                        return '' + number_format(value);
-                    }
-                },
-                gridLines: {
-                    color: "rgb(234, 236, 244)",
-                    zeroLineColor: "rgb(234, 236, 244)",
-                    drawBorder: false,
-                    borderDash: [2],
-                    zeroLineBorderDash: [2]
-                }
-            }],
-        },
-        legend: {
-            display: false
-        },
-        tooltips: {
-            titleMarginBottom: 10,
-            titleFontColor: '#6e707e',
-            titleFontSize: 14,
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            caretPadding: 10,
-            callbacks: {
-                label: function (tooltipItem, chart) {
-                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                    return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-                }
-            }
-        },
+}
+
+function createChartLine(histDataLocal) {
+
+    var count = [0, 0, 0, 0];
+    var voltArray = [0, 0, 0, 0];
+    var tempArray = [0, 0, 0, 0];
+
+    for (var i = 0; i < histDataLocal.length; i++) {
+        if (parseFloat(histDataLocal[i].voltage) == NaN) {
+            parseFloat(histDataLocal[i].voltage) == 0;
+        }
+        if (histDataLocal[i].turbineId == 1) {
+            count[0] += 1;
+            voltArray[0] += parseFloat(histDataLocal[i].voltage);
+            tempArray[0] += parseFloat(histDataLocal[i].temp);
+        }
+        else if (histDataLocal[i].turbineId == 2) {
+            count[1] += 1;
+            voltArray[1] += parseFloat(histDataLocal[i].voltage);
+            tempArray[1] += parseFloat(histDataLocal[i].temp);
+        }
+        else if (histDataLocal[i].turbineId == 3) {
+            count[2] += 1;
+            voltArray[2] += parseFloat(histDataLocal[i].voltage);
+            tempArray[2] += parseFloat(histDataLocal[i].temp);
+        }
+        else if (histDataLocal[i].turbineId == 4) {
+            count[3] += 1;
+            voltArray[3] += parseFloat(histDataLocal[i].voltage);
+            tempArray[3] += parseFloat(histDataLocal[i].temp);
+        }
     }
-});
 
 
+    var volLen = [];
+    for (var j = 0; j < tempArray.length; j++) {
+
+        volLen[j] = j;
+    }
+    console.log(tempArray);
+    console.log(voltArray);
+    console.log(count);
+    tempArray[0] = 0;
+    tempArray[1] = tempArray[1] / count[0];
+    tempArray[2] = tempArray[2] / count[1];
+    tempArray[3] = tempArray[3] / count[2];
+
+    console.log(tempArray);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+    // Pie Chart Example
+    var ctx = document.getElementById("myPieChart");
+    var myBarChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["1", "2", "3"],
+            datasets: [{
+                label: "Temperature",
+                hoverBackgroundColor: "#2e59d9",
+                borderColor: "#4e73df",
+                data: tempArray,
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'Temperature'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 150,
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function (value, index, values) {
+                            return '' + number_format(value);
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+                callbacks: {
+                    label: function (tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+                    }
+                }
+            },
+        }
+    });
+}
